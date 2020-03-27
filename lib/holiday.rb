e9c77794305d892fd79a93b71c31bd731e0ce7fb
Samiylo Kryshu <skryshu@gmail.com>
2020-03-27 20:13:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
     holiday_hash.each do |season, holidays|
-    puts season.capitalize
+    puts season.capitalize.to_s
 
     holidays.each do |holiday, items|
       puts holiday.capitalize
