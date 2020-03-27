55408cd132b1d1c5604478c5929a890b1841b468
Samiylo Kryshu <skryshu@gmail.com>
2020-03-27 19:50:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,10 +64,10 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
    holiday_hash.each do |season|
-    
+    puts season
 
     season.each do |holiday, item|
-      puts season
+      
       
     end
 
